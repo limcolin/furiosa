260663772ee560cb11442c990189284c160193d0
limcolin <limcolin@hotmail.com>
2019-01-31 15:00:20 +1100

    Removed requests/index route as this is rendered on campaigns/index instead

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ const routes = require('next-routes')();
 
 routes.add('/campaigns/new', '/campaigns/new');
 routes.add('/campaigns/:address', '/campaigns/show');
-routes.add('/campaigns/:address/requests', '/campaigns/requests/index');
 routes.add('/campaigns/:address/requests/new', '/campaigns/requests/new');
 routes.add('/freelancers', '/freelancers/index');
 routes.add('/freelancers/new', '/freelancers/new');
